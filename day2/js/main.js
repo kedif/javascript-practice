94ece8f888c54ab7b011eb5562d28f53a7ea081f
kedif <kedest_2006@yahoo.com>
2022-04-10 22:38:45 -0700

    commit file

---
+++ day2/js/main.js
@@ -0,0 +1,8 @@
+console.log ('Supercalifragilisticexpialidocious'.length);
+
+console.log (40);
+
+console.log (1234567891289012)
+let mysecrteDataType = '40';'1234567891289012';
+
+console.log (typeof mysecretDataType);
